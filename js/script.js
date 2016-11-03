98d4f6b871c7b5c1167caff914b804f21c6990ef
Jennifer Mead <jennifer.e.mead@gmail.com>
2016-11-03 14:06:05 -0700

    Worked on attaching markers to the filter

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
-//this creats a variable called map
+//This creats a variable called map
 var map;
 
-//this is the model
+
+//This is the model
 //This array of objects holds the location info
 var locationInfo = [
   {name: 'Ca Momi Osteria', latlong: {lat: 38.2985, lng: -122.2866}, address: '1141 1st St, Napa, CA 94559'},
@@ -20,36 +21,30 @@ function initMap() {
     zoom: 17
   });
 
-  //Now we are putting the markers on the initial map
-  //This blank array will hold all of the markers info
-  var markersArray = [];
-
-  //Creates the infoWindow, keep outside of for loop becuase you only need to make it once
+  //The creates the info window
   var infowindow = new google.maps.InfoWindow({
   });
 
-  //This for loop pushes all of the markers into the array
+  //This for loop is used to create new marker properties and push them into each object in the locationInfo array, making them properties of all of the location objects
   for( i = 0; i<locationInfo.length; i++){
-    //Creation of the markers
-    var marker = new google.maps.Marker({
+    //Creates each marker as a property of an object in the locationInfo array
+    locationInfo[i].marker = new google.maps.Marker({
       position: locationInfo[i].latlong,
       map: map,
       title: locationInfo[i].name,
       address: locationInfo[i].address
     });
 
-    //Pushes the markers into an array of objects called markers
-    markersArray.push(marker);
-
-    //This adds a click event to the markers that causes the infoWindow to open upon clicking. I think the marker is an object and we are adding properties to that object? Thats why we can do this after pushing it in the array
-    marker.addListener('click', function(){
-      //Why do you need the "this" again? I dont really understand this part or how "marker" in the later function can just replace this. I think "this" is just referring to the marker object. Its "self"
+    //This adds a click event to the marker properties that causes the infoWindow to open upon clicking. It doesn't contain the content yet though.
+    locationInfo[i].marker.addListener('click', function(){
+      //Now we are calling the populateInfoWindow function that we set up later
       populateInfoWindow(this, infowindow);
     });
   }
 }
 
-//I think the infoWindow and the marker are both objects
+console.log(locationInfo);
+
 //This function makes sure that the infowindow appears and sets the content to the correct title, it also clears the window content if the info window is closed
 function populateInfoWindow(marker, infowindow) {
   // This just makes sure the window is not already open
@@ -65,16 +60,15 @@ function populateInfoWindow(marker, infowindow) {
   }
 }
 
-
-
-// This is a the viewmodel
+// This is a the viewmodel for the KO code
 function appViewModel() {
   var self = this;
-  //Is this a property of the appViewModel? Its declaring it as an observable
+  //This is creating a reference to the filter data-bind in the HTML
   self.filter = ko.observable();
 
-  //Adding as a property a KO array
+  //Adding as a property? a KO observable array
   self.myObservableArray = ko.observableArray();
+
   //This for loop is what actually pushes the locationInfo objects into the observable array
   for(var i=0; i<locationInfo.length; i++){
     self.myObservableArray.push(locationInfo[i]);
@@ -83,8 +77,9 @@ function appViewModel() {
   //this is a method that is being added to the function. Its a KO computed observable
   self.filteredItems = ko.computed(function() { console.log(self);
 
-    //if no value has been entered, just return the observable array
-    if (!filter) {
+    //if no value has been entered, just return the observable array and set the marker to visable
+    if (!self.filter()) {
+        self.myObservableArray().marker.setVisible(true);
         return self.myObservableArray();
     } else {
          //the variable filter is holding the results of the user input into filter and then converting it to all lower case
@@ -95,31 +90,28 @@ function appViewModel() {
             //indexOf returns the index of the first occurance of a query value. If there is no query value in the string, indexOf returns a -1.
             //Thus, if you have a match at all, the result must be 0 or greater becuase 0 is the lowest index number. So if you have any result, it will be greater than -1 and so returns true. Otherwise it returns false
             //do we need to do toLowerCase twice?
+            var result = item.name.toLowerCase().indexOf(filter);
+
+              if (result !== 0) {
+                item.marker.setVisible(false);
+              } else {
+                 item.marker.setVisible(true);
+              }
+
             return item.name.toLowerCase().indexOf(filter) > -1;
         });
     }
   //do we still need appViewModel here?
-  }, appViewModel);
+  });
 
   };
 
-// Activates knockout.js
-ko.applyBindings(new appViewModel());
+  // Activates knockout.js
+  ko.applyBindings(new appViewModel());
 
+  //we need two different functions, one that returns the list basede on the filter and once that returns the markers based on the filter
+  //they both initially load if there is nothing in the filter, but as sooon as their is something in the filter, things are only returned to the list if they are true and markers are only
+  //shown if they are true
 
- 
-
-//A list of the names of the markers should appear on the left side. This requires KO and it means I have to populate the DOM. Review the KO stuff briefly and read over the rules thouroughly!!
-
-//Provide a filter option that uses an input field to filter both the list view and the map markers displayed by default on load, IE: if you filter by the word park, anything without that word should go away and only markers with that word should stay
-
-
-//Add functionality using third-party APIs to provide information when a map marker or list view entry is clicked
-
-
-//Add functionality to animate a map marker when either the list item associated with it or the map marker itself is selected.
-//Add functionality to open an infoWindow with the information when either a location is selected from the list view or its map marker is selected directly.
-
-
-
-//error handling
+  //error handling
+  //I think the problem is that myObservableArray is not connected to the other function?
