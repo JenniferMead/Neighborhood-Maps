390c52d97a4a621869bdffcad5130ba4eeb18c07
Jennifer Mead <jennifer.e.mead@gmail.com>
2016-11-17 13:56:11 -0800

    Added the final code and cleaned everything up

--- js/script.js
+++ js/script.js
@@ -14,46 +14,40 @@ var locationInfo = [
   {name: 'Alexis Baking Company', latlong: {lat: 38.2955, lng: -122.2888}, address: '1517 3rd St, Napa, CA 94559', businessId: 'alexis-baking-company-napa'}
 ];
 
+
 //this function initializes the map
 function initMap() {
   var text = 'Google maps';
   var paragraph = document.createElement('p');
-  //If I did this right its error handling for when the google maps script hasn't loaded yet. I got help with this idea here: http://codepen.io/SittingFox/pen/yYbwwE?editors=001
-  //I don't know how I can test this one though becuase it doesn't seem to work when the internet is full disconnected so I don't know if its working or not
-  //I based it off of suggestions found here: https://discussions.udacity.com/t/handling-google-maps-in-async-and-fallback/34282?_ga=1.32303357.258409296.1477275507
-  //I don't know if I even need this actually
+  //This is error handling for when the google maps script hasn't loaded yet. I got help with this idea here: http://codepen.io/SittingFox/pen/yYbwwE?editors=001 and here: https://discussions.udacity.com/t/handling-google-maps-in-async-and-fallback/34282?_ga=1.32303357.258409296.1477275507
   if(typeof google === 'undefined'){
     text += 'is not loaded';
     paragraph.innerHTML = text;
-
-    // Add paragraph to page
+    //Add paragraph to page explaining that Google maps is not loaded
     var body = document.getElementsById('mapWrapper');
     body.appendChild(paragraph);
   }
 
   else{
-  // This constructor creates the new map at the chosen location
-  map = new google.maps.Map(document.getElementById('map'), {
-    //I don't think I need this line anymore if I am creating the center below and setting it to the center of the bounds
-    //center: {lat: 38.2970, lng: -122.2876},
+    // This constructor creates the new map at the chosen location
+    map = new google.maps.Map(document.getElementById('map'), {
     zoom: 17
-  });
+    });
 
-  //The creates the info window
-  var infowindow = new google.maps.InfoWindow({
-  });
-  //This creates the lat long boundries
-  var bounds = new google.maps.LatLngBounds();
+    //The creates the info window
+    var infowindow = new google.maps.InfoWindow({
+    });
+    //This creates the lat long boundries
+    var bounds = new google.maps.LatLngBounds();
 
-  //This for loop is used to create new marker properties and push them into each object in the locationInfo array, making them properties of all of the location objects
-  for( i = 0; i<locationInfo.length; i++){
-    //Creates each marker as a property of an object in the locationInfo array
-    locationInfo[i].marker = new google.maps.Marker({
+    //This for loop is used to create new marker properties and push them into each object in the locationInfo array, making them properties of all of the location objects
+    for( i = 0; i<locationInfo.length; i++){
+      //Creates each marker as a property of an object in the locationInfo array
+      locationInfo[i].marker = new google.maps.Marker({
       position: locationInfo[i].latlong,
       map: map,
       title: locationInfo[i].name,
       address: locationInfo[i].address,
-      //This is only doing the initial animation on load, the drop effect. Not bouncing
       animation: google.maps.Animation.DROP
     });
 
@@ -65,36 +59,38 @@ function initMap() {
 
     //This adds the click event that calls the function in control of the animation of the marker
     locationInfo[i].marker.addListener('click', function(){
-     toggleBounce(this);
+      toggleBounce(this);
     });
 
     bounds.extend(locationInfo[i].marker.position);
 
-  }
-  // Extend the boundaries of the map for each marker
+    }
+    // Extend the boundaries of the map for each marker
     map.fitBounds(bounds);
 
     //center the map to the geometric center of all markers. If I do this, aren't I just resetting the center I determined above when I made the map initially?
     map.setCenter(bounds.getCenter());
+  }
 }
-}
-//I don't know if the lat long bounds isn't working or if its doing everything it can. It definietly adjusts and keeps everything within the bounds as LONG as I don't move the screen. If I move the screen though,
-//it will not move it back to the marker. Also, it does not adjust the zoom based on how many markers are in the screen and how far apart they are
-//Actualy Im pretty sure its not adjusting becuase its staying the center of the markers but it doesn't know when a marker is being filtered and thus removed from the list? So its not updating?
 
-console.log(locationInfo);
 
+//This function toggles the marker between bouncing and not bouncing
 function toggleBounce(marker) {
+  //This makes sure that all markers have stopped bouncing first
+  for(i=0; i<locationInfo.length; i++){
+      locationInfo[i].marker.setAnimation(null);
+  };
   //If the marker is already animated, stop animation
   if (marker.getAnimation() !== null) {
     marker.setAnimation(null);
   } else {
-    //Otherwise, set animation
+    //Otherwise, set animation on this marker
     marker.setAnimation(google.maps.Animation.BOUNCE);
   }
 }
 
-//This function makes sure that the infowindow appears and sets the content to the correct title, it also clears the window content if the info window is closed
+
+//This function makes sure that the infowindow appears and sets the content to the correct information, it also clears the window content if the info window is closed
 function populateInfoWindow(marker, infowindow) {
   // This just makes sure the window is not already open
   if (infowindow.marker != marker) {
@@ -112,65 +108,64 @@ function populateInfoWindow(marker, infowindow) {
 }
 
 
-// This is a the viewmodel for the KO code
+//This is a the viewmodel for the KO code
 function appViewModel() {
   var self = this;
   //This is creating a reference to the filter data-bind in the HTML
   self.filter = ko.observable();
-
-  //Adding as a property? a KO observable array
+  //Adding as a property a KO observable array
   self.myObservableArray = ko.observableArray();
-
   //This for loop is what actually pushes the locationInfo objects into the observable array
   for(var i=0; i<locationInfo.length; i++){
     self.myObservableArray.push(locationInfo[i]);
   }
 
-//Here is where I connect the list to the markers. I've added a click event on the DOM element that connects to this. whenever a list item is clicked, this function is run
-//It basically triggers all of the click events on the marker
-//Shouldn't this actually be in the for loop since we are adding functionalitiy to each of the list items??
- self.listClicker = function(locationInfo){
-   google.maps.event.trigger(locationInfo.marker, 'click')
- };
+  //Here is where I connect the list to the markers. I've added a click event on the DOM element that connects to this. whenever a list item is clicked, this function is run
+  //It basically triggers all of the click events on the marker
+  self.listClicker = function(locationInfo){
+    google.maps.event.trigger(locationInfo.marker, 'click')
+  };
 
-  //this is a method that is being added to the function. Its a KO computed observable
+  //This is a method that is being added to the function. Its a KO computed observable
   self.filteredItems = ko.computed(function() { console.log(self);
 
-    //if no value has been entered, just return the observable array and set the marker to visable
-    if (!self.filter()) {
-      // loop through locations
-      self.myObservableArray().forEach( function (location) {
-      // if marker poperty exists its sets the visibility to true. It won't exist on load, but it WILL exist after the page has loaded and you have typed in the filter box and then cleared it
+  //if no value has been entered, just return the observable array and set the marker to visable
+  if (!self.filter()) {
+    // loop through locations
+    self.myObservableArray().forEach( function (location) {
+    // if marker poperty exists its sets the visibility to true. It won't exist on load, but it WILL exist after the page has loaded and you have typed in the filter box and then cleared it
       if (location.marker) {
         location.marker.setVisible(true);
       }
-      });
+    });
       return self.myObservableArray();
-    }
-    else {
-         //the variable filter is holding the results of the user input into filter and then converting it to all lower case
-         var filter = self.filter().toLowerCase();
-         //returns an array that contains only those items in the array that is being filtered that pass the true/false test inside the filter
-         return ko.utils.arrayFilter(self.myObservableArray(), function(item) {
-            //Holds the result of the filter in a variable that is converted to a number based on how .indexOf works
-            //indexOf returns the index of the first occurance of a query value. If there is no query value in the string, indexOf returns a -1.
-            var result = item.name.toLowerCase().indexOf(filter);
-              //If there were no matches between the filter and the list, hide the marker
-              if (result < 0) {
-                item.marker.setVisible(false);
-                //If there were matches, show the marker
-              } else {
-                 item.marker.setVisible(true);
-              }
+  }
 
-            //Based on how indexOf works, if you have a match at all, the result must be 0 or greater becuase 0 is the lowest index number.
-            //So if you have any result, it will be greater than -1 and so returns true. Otherwise it returns false
-            return item.name.toLowerCase().indexOf(filter) > -1;
-        });
+  else {
+    //the variable filter is holding the results of the user input into filter and then converting it to all lower case
+    var filter = self.filter().toLowerCase();
+      //returns an array that contains only those items in the array that is being filtered that pass the true/false test inside the filter
+      return ko.utils.arrayFilter(self.myObservableArray(), function(item) {
+      //Holds the result of the filter in a variable that is converted to a number based on how .indexOf works
+      //indexOf returns the index of the first occurance of a query value. If there is no query value in the string, indexOf returns a -1.
+      var result = item.name.toLowerCase().indexOf(filter);
+      //If there were no matches between the filter and the list, hide the marker
+        if (result < 0) {
+          item.marker.setVisible(false);
+          //If there were matches, show the marker
+        }
+        else {
+          item.marker.setVisible(true);
+        }
+
+        //Based on how indexOf works, if you have a match at all, the result must be 0 or greater becuase 0 is the lowest index number.
+        //So if you have any result, it will be greater than -1 and so returns true. Otherwise it returns false
+        return item.name.toLowerCase().indexOf(filter) > -1;
+      });
     }
   });
+};
 
-  };
 
 //Authentication for YELP API
 //Generates a random number and returns it as a string for OAuthentication
@@ -185,11 +180,8 @@ var secret_key = "MUurURfv82G-0QGpjQImc04gi8A";
 var secret_token = "IlHuDvpCNL183-nePGIDH4Tb69g";
 
 
-
-
 //This is the function that contains the actual ajax request, but it isn't called until later
 var yelpCaller = function(place){
-
   //Url variable
   var yelp_url = "https://api.yelp.com/v2/business/" + place.businessId;
 
@@ -231,7 +223,7 @@ var yelpCaller = function(place){
       console.log("fail");
 
     }
-    };
+  };
 
 
   // Send AJAX query via jQuery library. This is what is actually sending the request
